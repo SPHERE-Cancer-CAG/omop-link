d03400ffe265d22d8b653b239bd1410c27ab2628
georgie <georgina.kennedy@unsw.edu.au>
2026-03-03 13:43:15 +1100

    profiles

--- src/omop_profiles/datamodel/omop_profiles.py
+++ src/omop_profiles/datamodel/omop_profiles.py
@@ -1,5 +1,5 @@
 # Auto generated from omop_profiles.yaml by pythongen.py version: 0.0.1
-# Generation date: 2026-03-03T13:41:31
+# Generation date: 2026-03-03T13:42:50
 # Schema: omop_profiles
 #
 # id: https://athena.ohdsi.org/search-terms/terms/omop_profiles
